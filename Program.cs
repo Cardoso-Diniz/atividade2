7ff2eb7b5602950f5fd4266a906319e277b23257
Cardoso-Diniz <leonardo030605@gmail.com>
2021-07-19 09:59:34 -0300

    terminei o trampo

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é o seu nome?");
+            string nomePessoa = Console.ReadLine();
+
+            Console.WriteLine("Qual é a sua idade?");
+            int idade = int.Parse(Console.ReadLine());
+
+           if (idade >=16)
+
+           {
+            Console.WriteLine("PARABÉNS, você possui a idade necessaria");
+           }
+           else
+           {
+               Console.WriteLine("Você não possui a idotnedade necessaria, você estará acompanhado com alguém maior de 18 anos sim/não?");
+               string resposta = Console.ReadLine().ToLower();
+
+               switch (resposta)
+             {
+                case "sim":
+                Console.WriteLine($"Então vocÊ pode participar,porém esteja acompanhando");
+                
+                break;
+
+                case "não":
+                Console.WriteLine($"Você não pode participar!!");
+                break;     
+
+                default:
+                break;
+              }
+            }
+            {   
+                }  
+        }
+    }
+}
